4b8abc62d79d660662760ed9a07f00bb472f0185
Ayden Mitchell <ayd3n1229@gmail.com>
2019-03-14 17:47:16 -0400

    added start for pebble weapons

--- src/main/java/aydenmitchell/pebblemod/init/ModItems.java
+++ src/main/java/aydenmitchell/pebblemod/init/ModItems.java
@@ -10,6 +10,7 @@ import aydenmitchell.pebblemod.util.Reference;
 import net.minecraft.init.SoundEvents;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.Item;
+import net.minecraft.item.Item.ToolMaterial;
 import net.minecraft.item.ItemArmor.ArmorMaterial;
 import net.minecraftforge.common.util.EnumHelper;
 
@@ -17,17 +18,18 @@ public class ModItems {
 	public static final List<Item> ITEMS = new ArrayList<Item>();
 	
 	//Material
-	public static final ArmorMaterial TOOL_PEBBLE = EnumHelper.addArmorMaterial("armor_pebble", Reference.MOD_ID + ":pebble", 6, new int[]{1, 2, 3, 1}, 15, SoundEvents.ITEM_ARMOR_EQUIP_GENERIC, 0.0F);
+	public static final ArmorMaterial ARMOR_PEBBLE = EnumHelper.addArmorMaterial("armor_pebble", Reference.MOD_ID + ":pebble", 6, new int[]{1, 2, 3, 1}, 15, SoundEvents.ITEM_ARMOR_EQUIP_GENERIC, 0.0F);
+	public static final ToolMaterial TOOL_WEAPON = EnumHelper.addToolMaterial("tool_pebble", 1, 131, 4.0F, 1.0F, 5);
 	
 	//Items
 	public static final Item PEBBLE = new ItemPebble("pebble");
 	public static final Item HANDFUL_PEBBLE = new ItemBase("handful_pebble");
 	public static final Item FOSSIL = new ItemBase("fossil");
-//	test item lol
+	
 	
 	//Armor
-	public static final Item HELMET_PEBBLE = new ArmorBase("helmet_pebble", TOOL_PEBBLE, 1, EntityEquipmentSlot.HEAD);
+	public static final Item HELMET_PEBBLE = new ArmorBase("helmet_pebble", ARMOR_PEBBLE, 1, EntityEquipmentSlot.HEAD);
 	
 	//Weapons
-//	public static final Item PEBBLE_WEAPON = new ItemBase("pebble_weapon");
+	
 }
